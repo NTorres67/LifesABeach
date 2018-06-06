cb48960be93e7e7322ac7a0c6b0b19c37ec8c0ac
NTorres67 <NTorres1967@gmail.com>
2018-06-06 15:06:50 -0400

    introduce children sleeping arrangement class

---
+++ LifesABeach/SleepingArrangements.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifesABeach
+{
+    class SleepingArrangements : Travel
+    {
+        //Determine children sleeping arrangements using an array
+    }
+}
